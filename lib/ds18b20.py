6cbda648b7f40367c061c37c8f762bd3f70372f1
thiago <thiagobordignon@gmail.com>
2018-04-10 22:51:17 +0100

    TBS002: need to find another way to test!

--- lib/ds18b20.py
+++ lib/ds18b20.py
@@ -2,6 +2,9 @@
 import os
 import json
 
+BASE_DIRECTORY = "/sys/bus/w1/devices"
+SLAVE_FILE = "w1_slave"
+
 def listSensors():
     sensors = []
     for i in os.listdir('/sys/bus/w1/devices'):
@@ -11,6 +14,18 @@ def listSensors():
 
 def read(sensor):
     location = '/sys/bus/w1/devices/' + str(sensor) + '/w1_slave'
+    tfile = open(location)
+    text = tfile.read()
+    tfile.close()
+    secondline = text.split("\n")[1]
+    temperaturedata = secondline.split(" ")[9]
+    temperature = float(temperaturedata[2:])
+    celsius = temperature / 1000
+    farenheit = (celsius * 1.8) + 32
+    return celsius, farenheit
+
+def read2(sensor):
+    location =  os.path.join(BASE_DIRECTORY,sensor, SLAVE_FILE)
     tfile = open(location)
     text = tfile.read()
     tfile.close()
--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+import os
 import lib.ds18b20 as ds18b20
 
 app = Flask(__name__)
@@ -13,6 +13,10 @@ def getAll():
 @app.route('/api/ds18b20/getTempetature/<sensor>')
 def getTempetature(sensor):
   return ds18b20.read(sensor)
+
+@app.route('/api/ds18b20/getTempetature2/<sensor>')
+def getTempetature2(sensor):
+  return ds18b20.read2(sensor)
 
 # Main #
 
