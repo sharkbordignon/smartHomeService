ce14c77c6465687512f76f55ee8fd9e4ff40c64f
thiago <thiagobordignon@gmail.com>
2018-04-15 00:06:24 +0100

    SH0012: bug with param

--- lib/sensors.py
+++ lib/sensors.py
@@ -3,9 +3,10 @@ sys.path.append('config')
 import setup_sensors as cfg
 
 def listSensors(status):
+  status = status.replace("'", "")
   list_sensors = cfg.setup["sensorsList"]
   if(status != None):
     return filter(lambda x: x['status'] == status, list_sensors)
   return list_sensors
 
-print listSensors('on')
+#print listSensors('on')
