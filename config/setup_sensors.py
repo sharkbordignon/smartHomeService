24277957edf450ca96c098ece064b0dfdd933b75
thiago <thiagobordignon@gmail.com>
2018-04-14 22:26:38 +0100

    SH0011: forgot that

---
+++ config/setup_sensors.py
@@ -0,0 +1,18 @@
+setup = {
+  'sensorsList' : [
+    {
+      'code': 'temp_ds18_01',
+      'name': '28-0317249f99ff',
+      'nickname': 'Temperature inside',
+      'type': 'temperature',
+      'status': 'on'
+    },
+    {
+      'code': 'temp_ds18_02',
+      'name': '28-031725301cff',
+      'nickname': 'Temperature outside',
+      'type': 'temperature',
+      'status': 'on'
+    }
+  ]
+}
