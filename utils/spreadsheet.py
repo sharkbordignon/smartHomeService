d940d322d56d25dce01fa3b8a8717221b87b0eb0
thiago <thiagobordignon@gmail.com>
2018-04-14 22:32:07 +0100

    SH0011: fix path

--- utils/spreadsheet.py
+++ utils/spreadsheet.py
@@ -3,15 +3,15 @@ import pandas as pd
 
 def insertRow(name, datetime, temperature):
   #authorization
-  gc = pygsheets.authorize(service_file='../config/smartHome-afc01dc3da78.json')
-  
+  gc = pygsheets.authorize(service_file='config/smartHome-afc01dc3da78.json')
+
   #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
   sh = gc.open('temperature_sensor')
 
-  #select the first sheet 
+  #select the first sheet
   wks = sh[0]
 
   wks.append_table(values=[name,datetime,temperature])
 
-  #update the first sheet with df, starting at cell B2. 
+  #up`date the first sheet with df, starting at cell B2.
   #wks.set_dataframe(df,(1,1))
