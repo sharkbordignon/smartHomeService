e317e0abdcda7a2bf5250d4bc9dedc86f0025bb9
thiago <thiagobordignon@gmail.com>
2018-04-15 00:21:42 +0100

    SH0012: forgot to return as json

--- lib/sensors.py
+++ lib/sensors.py
@@ -2,10 +2,12 @@ import sys
 sys.path.append('config')
 import setup_sensors as cfg
 
+import json
+
 def listSensors(status):
   list_sensors = cfg.setup["sensorsList"]
   if(status != None):
     return filter(lambda x: x['status'] == status, list_sensors)
-  return list_sensors
+  return json.dumps(list_sensors)
 
 #print listSensors('on')
