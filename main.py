170587aa6bc1161a44c9e97eb733ef0baf488d1d
thiago <thiagobordignon@gmail.com>
2018-04-15 00:32:03 +0100

    SH0012: convert json to object and using real value

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import spreadsheet as ss
 
 import lib.sensors as sensors
 import datetime
+import json
 
 if __name__ == '__main__':
   try:
-    available_sensors = sensors.listSensors('on')
+    available_sensors = json.loads(sensors.listSensors('on'))
     temperature_sensores = filter(lambda x: x['type'] == 'temperature', available_sensors)
     if temperature_sensores != []:
       import lib.ds18b20 as ds18b20
       for x in temperature_sensores:
-        temperature = 12
+        temperature = ds18b20.read(x['name'])
         ss.insertRow(x['nickname'], str(datetime.datetime.now()), temperature)
   except KeyboardInterrupt:
     print 'ERROOORRRRRR'
