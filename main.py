691068667926e9fb24e19f188f99bd5b41e0d8dc
thiago <thiagobordignon@gmail.com>
2018-04-15 13:27:35 +0100

    SH0014: add loop and start record

--- main.py
+++ main.py
@@ -6,15 +6,18 @@ import spreadsheet as ss
 import lib.sensors as sensors
 import datetime
 import json
+import time
 
 if __name__ == '__main__':
   try:
-    available_sensors = json.loads(sensors.listSensors('on'))
-    temperature_sensores = filter(lambda x: x['type'] == 'temperature', available_sensors)
-    if temperature_sensores != []:
-      import lib.ds18b20 as ds18b20
-      for x in temperature_sensores:
-        temperature = ds18b20.read(x['name'])
-        ss.insertRow(x['nickname'], str(datetime.datetime.now()), temperature)
+    while true:
+      available_sensors = json.loads(sensors.listSensors('on'))
+      temperature_sensores = filter(lambda x: x['type'] == 'temperature', available_sensors)
+      if temperature_sensores != []:
+        import lib.ds18b20 as ds18b20
+        for x in temperature_sensores:
+          temperature = json.loads(ds18b20.read(x['name']))[0][1]
+          ss.insertRow(x['nickname'], str(datetime.datetime.now()), temperature)
+      time.sleep(60)
   except KeyboardInterrupt:
     print 'ERROOORRRRRR'
